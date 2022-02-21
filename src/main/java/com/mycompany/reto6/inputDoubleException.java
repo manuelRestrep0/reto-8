50c8bc5dcc4a5f5ff26830412f080f2a5005e822
manuelRestrep0 <94997816+manuelRestrep0@users.noreply.github.com>
2022-02-20 21:40:12 -0600

    commint codigo reto 6

---
+++ src/main/java/com/mycompany/reto6/inputDoubleException.java
@@ -0,0 +1,7 @@
+package com.mycompany.reto6;
+
+public class inputDoubleException extends Exception {
+    public inputDoubleException(){
+        super("El dato ingresado no es de tipo double");
+    }
+}
